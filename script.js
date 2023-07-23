ae9f78c397f7c453423b65f14c20baea71bc244c
Sam <73436924+samwise-webdev@users.noreply.github.com>
2023-07-23 10:38:01 -0400

    added the slice/case change to the name variable and that worked

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function capitalize(submission, secondWord){
-    submission = submission.slice(0,1).toUpperCase() + submission.slice(1).toLowerCase();  
-    secondWord = secondWord.slice(0,1).toUpperCase() + secondWord.slice(1).toLowerCase();    
-    console.log(submission, secondWord);  
-    return [submission, secondWord]; }
-const capBtn = document.querySelector('#capMe');
-capBtn.addEventListener('click', capitalize);
+// function capitalize(firstWord, secondWord){
+//     firstWord = firstWord.slice(0,1).toUpperCase() + firstWord.slice(1).toLowerCase();  
+//     secondWord = secondWord.slice(0,1).toUpperCase() + secondWord.slice(1).toLowerCase();    
+//     console.log(firstWord, secondWord);  
+//     return [firstWord, secondWord]; }
+// const capBtn = document.querySelector('#capMe');
+// capBtn.addEventListener('click', capitalize);
 
 const form = document.querySelector('#capitalizer');
 const input = document.querySelector('#capitalizeMe');
@@ -12,7 +12,8 @@ const result = document.querySelector('#result');
 
 const submission = form.addEventListener('submit', function (e) {
     e.preventDefault(); //prevent redirect
-    const name = input.value; //set the input to a variable
+    let name = input.value; //set the input to a variable
+    name = name.slice(0,1).toUpperCase() + name.slice(1).toLowerCase();
     const p = document.createElement('p'); //add a p element to the page
     p.innerText = name; //set the text to the input value from name variable
     result.append(p); //add the input value to the page
