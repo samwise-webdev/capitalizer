6ba33661aa4ed479284d1f5be5911b71cea8e823
Sam <73436924+samwise-webdev@users.noreply.github.com>
2023-07-23 10:34:26 -0400

    making sure capitalize function works

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-// function capitalize(firstWord, secondWord){
-//     firstWord = firstWord.slice(0,1).toUpperCase() + firstWord.slice(1).toLowerCase();
-    
-//     secondWord = secondWord.slice(0,1).toUpperCase() + secondWord.slice(1).toLowerCase();
-    
-//     console.log(firstWord, secondWord);
-    
-//     return [firstWord, secondWord]; }
-
-// const capBtn = document.querySelector('#capMe');
-
-// capBtn.addEventListener('click', capitalize);
+function capitalize(firstWord, secondWord){
+    firstWord = firstWord.slice(0,1).toUpperCase() + firstWord.slice(1).toLowerCase();  
+    secondWord = secondWord.slice(0,1).toUpperCase() + secondWord.slice(1).toLowerCase();    
+    console.log(firstWord, secondWord);  
+    return [firstWord, secondWord]; }
+const capBtn = document.querySelector('#capMe');
+capBtn.addEventListener('click', capitalize);
+capitalize('bob', 'barker');
 
 const form = document.querySelector('#capitalizer');
 const input = document.querySelector('#capitalizeMe');
